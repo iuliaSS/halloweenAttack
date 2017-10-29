0b3ff3e9165fa8d8c0814d0fca400fc9601bfb87
Iulia Sava <fireflies_jar@yahoo.com>
2017-10-29 21:30:58 +0200

    HAL-12 Dynamic elements

---
+++ HalloweenAttack/Assets/Raggedy Witch - Halloween/Scripts/GeneratingEnemies.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneratingEnemies : MonoBehaviour {
+
+	public GameObject prefab;
+	public float time;
+
+	// Use this for initialization
+	IEnumerator Start () {
+		int[] y;
+		int random;
+		y = new int[] {-10, 0, 10};
+		yield return new WaitForSeconds (time);
+	
+		for (int i = 0; i < 10; i++)
+		{
+			yield return new WaitForSeconds (15);
+			Instantiate(prefab);
+			random = Random.Range(0, 3);
+			prefab.transform.transform.position = new Vector3 (0, y[random], 0);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ HalloweenAttack/Assets/Raggedy Witch - Halloween/Scripts/Movement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	public float speed;
+	public float xaxis;
+
+	// Use this for initialization
+	void Start () {
+		speed = -0.03f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(speed, 0f, 0f);
+	}
+}
