257b115ec220a3dd17008f99a3ec73a85649f0dd
iuliaSS <fireflies_jar@yahoo.com>
2017-11-05 20:52:56 +0200

    Design fixes and HAL-12 Dynamic elements HAL-16 Projectile movement

---
+++ HalloweenAttack/Assets/halloweenAttack/Scripts/Bullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    public GameObject bulletPrefab;
+    public float speed;
+    public float time;
+    private float nextActionTime;
+
+    // Use this for initialization
+    void Start () {
+        speed *= -1;
+        nextActionTime = Time.time;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.time >= nextActionTime)
+        {
+            nextActionTime += time;
+            var instanceBullet = (GameObject)Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+            instanceBullet.GetComponent<Rigidbody>().velocity = instanceBullet.transform.right * speed;
+            Destroy(instanceBullet, 15.0f);
+        }
+    }
+}
---
+++ HalloweenAttack/Assets/halloweenAttack/Scripts/Movement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	private float speed;
+
+	// Use this for initialization
+	void Start () {
+		speed = -0.03f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(speed, 0f, 0f);
+	}
+}
