a1199ec99c4afec14c590b118d2e3d2a7c7e6cd6
Iulia Sava <fireflies_jar@yahoo.com>
2017-11-11 18:16:36 +0200

    Collisions and rotating pumpkin

---
+++ HalloweenAttack/Assets/halloweenAttack/Scripts/Collisions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collisions : MonoBehaviour {
+	
+	void OnTriggerEnter(Collider collider) {
+		if (collider.gameObject.tag == "Defensive") {
+			Destroy(collider.gameObject);
+		}
+	}
+}
