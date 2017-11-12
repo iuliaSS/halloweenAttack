2f982595729343a24a7d8592fd484949b2853511
Iulia Sava <fireflies_jar@yahoo.com>
2017-11-12 13:16:33 +0200

    Life update

---
+++ HalloweenAttack/Assets/halloweenAttack/Scripts/EnemyLife.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLife : MonoBehaviour {
+
+	public int life;
+
+	void OnTriggerEnter(Collider collider) {
+		if (collider.gameObject.tag == "BlueBullet") {
+
+			life--;
+			if (life == 0)
+				Destroy (this.gameObject);
+			Destroy (collider.gameObject);
+		}
+	}
+}
