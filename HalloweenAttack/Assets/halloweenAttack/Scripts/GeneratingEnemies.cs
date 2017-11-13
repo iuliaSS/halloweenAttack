fa82a88e4659a37b939609c1b393b734c867449c
Matei Alexandru <alexandrumatei3693@gmail.com>
2017-11-14 01:07:41 +0200

    Add different levels

--- HalloweenAttack/Assets/halloweenAttack/Scripts/GeneratingEnemies.cs
+++ HalloweenAttack/Assets/halloweenAttack/Scripts/GeneratingEnemies.cs
@@ -3,17 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GeneratingEnemies : MonoBehaviour {
-
+    
 	public GameObject prefab;
 	public float time;
+    public int generatedEnemies;
+    float[] y;
 
-	// Use this for initialization
-	IEnumerator Start () {
-		float[] y;
+    // Use this for initialization
+    IEnumerator Start () {
 		int random;
 		y = new float[] {2.8f, 0.8f, -1.3f};
 		yield return new WaitForSeconds (time);
-	
+        
 		for (int i = 0; i < 10; i++)
 		{
 			yield return new WaitForSeconds (15);
@@ -22,10 +23,31 @@ public class GeneratingEnemies : MonoBehaviour {
 			prefab.transform.position = new Vector3 (5, y[random], -0.1f);
 			Destroy(instance, 40);
 		}
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        LevelScript.levelValue = "2";
+        for (int i = 0; i < 20; i++)
+        {
+            yield return new WaitForSeconds(12);
+            GameObject instance = Instantiate(prefab);
+            random = Random.Range(0, 3);
+            prefab.transform.position = new Vector3(5, y[random], -0.1f);
+            Destroy(instance, 40);
+        }
+
+        LevelScript.levelValue = "3";
+        for (int i = 0; i < 50; i++)
+        {
+            yield return new WaitForSeconds(6);
+            GameObject instance = Instantiate(prefab);
+            random = Random.Range(0, 3);
+            prefab.transform.position = new Vector3(5, y[random], -0.1f);
+            Destroy(instance, 40);
+        }
+
+        LevelScript.levelValue = "FINAL JOC";
+    }
+
+    // Update is called once per frame
+    void Update () {
+        
+    }
 }
---
+++ HalloweenAttack/Assets/halloweenAttack/Scripts/LevelScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelScript: MonoBehaviour
+    {
+        public static string levelValue = "1";
+        Text level;
+
+
+        // Use this for initialization
+        void Start()
+        {
+            level = GetComponent<Text>();
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            level.text = "Level: " + levelValue;
+
+        }
+    }
